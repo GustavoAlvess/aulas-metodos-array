b9e3451913f29806d43841de06a56c772cf96d57
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 11:12:00 -0300

    feat: adiciona conteúdo a arquivo metodo-map.js

---
+++ scripts/metodo-map.js
@@ -0,0 +1,12 @@
+/* Cria um novo array a partir da transformação do array original */
+
+let playlist = ['Funk', 'Hip-Hop', 'Pop'];
+
+let playlistEmCaps = playlist.map(musica => musica.toUpperCase());
+
+/* nomeVariavel.map(novaVariável => novaVariavél.toUpperCase()
+
+to(Upper,Lower, String or LocalUpper)Case()
+*/
+
+console.log("Playlist em maiúsculas:", playlistEmCaps);
