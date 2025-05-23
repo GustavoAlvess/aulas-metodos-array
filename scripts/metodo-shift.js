83ed40fc909e7513b5ae6f3cdf433acff8ce54e0
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 10:07:04 -0300

    feat: cria arquivo metodo-unshift.js

--- scripts/metodo-shift.js
+++ scripts/metodo-shift.js
@@ -1,11 +1,11 @@
-let playlist = ['Funk', 'HipHop', 'Pop']
+let playlist = ["Funk", "HipHop", "Pop"];
 
-console.log('Antes do shift');
+console.log("Antes do shift");
 console.table(playlist);
 
 // removendo o primeiro estilo
 let estiloRemovido = playlist.shift();
 
-console.log('Depois do shift');
+console.log("Depois do shift");
 console.table(playlist);
 console.log("Estilo removido: ", estiloRemovido);
