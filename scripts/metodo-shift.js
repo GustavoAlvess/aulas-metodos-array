a0ed6bca19764f4e861594b9985b01b5d6ab2d33
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 10:12:10 -0300

    feat: adiciona conteúdo a arquivo metodo-shift.js

--- scripts/metodo-shift.js
+++ scripts/metodo-shift.js
@@ -3,7 +3,7 @@ let playlist = ["Funk", "HipHop", "Pop"];
 console.log("Antes do shift");
 console.table(playlist);
 
-// removendo o primeiro estilo
+// removendo o primeiro estilo da lista
 let estiloRemovido = playlist.shift();
 
 console.log("Depois do shift");
