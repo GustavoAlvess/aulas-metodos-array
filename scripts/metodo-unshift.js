11ac58b70704c3022c6bfe23947ace371d3b4be3
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 10:10:23 -0300

    feat: adiciona conteúdo a arquivo metodo-unshift.js

---
+++ scripts/metodo-unshift.js
@@ -0,0 +1,11 @@
+let playlist = ["Funk", "HipHop", "Pop"];
+
+console.log("Antes do unshift");
+console.table(playlist);
+
+// adicionando estilo na primeira posição do índice
+let estiloRemovido = playlist.unshiftshift("Rock");
+
+console.log("Depois do unshift");
+console.table(playlist);
+console.log("Estilo adicionado: ", estiloRemovido);
