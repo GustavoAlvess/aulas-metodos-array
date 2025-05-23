29e1eeda30d1da846d1e0dd9243926af7f88cc96
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 09:05:51 -0300

    feat: Adiciona conteúdo a arquivo metodo-push.js

---
+++ scripts/metodo-push.js
@@ -0,0 +1,9 @@
+let playlist = ['Funk', 'HipHop', 'Pop']
+
+console.log('Antes do push:');
+console.table(playlist);
+
+playlist.push('Trap')
+
+console.log('Depois do push:');
+console.table(playlist);
