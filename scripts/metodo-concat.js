2030ba69ad4fc11fad66626db3241c8ab77b5461
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 11:04:04 -0300

    feat: adiciona conteudo a arquivo metodo-concat.js

---
+++ scripts/metodo-concat.js
@@ -0,0 +1,13 @@
+/*
+Combina dois ou mais arrays
+*/
+
+let playlist1 = ['Funk', 'Hip-Hop', 'Pop'];
+
+let playlist2 = ['Rock', 'Jazz'];
+
+console.log("Playlist combinada:", playlist1.concat(playlist2));
+
+/*
+nomeVariavel1.concat(nomeVariavel2);
+*/
