653d6c346ebd59a8e4f5ff875c050bfa2843c2c4
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 10:42:55 -0300

    feat: adiciona conteúdo a arquivo metodo-join.js

---
+++ scripts/metodo-join.js
@@ -0,0 +1,10 @@
+// Converte o arquivo em string
+
+let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Trap'];
+
+console.log("Playlist como string:", playlist.join(', '));  
+
+// nomeVariavel.join( aqui dentro você coloca o que vai estar separando os elementos do array quando for convertido em string)
+
+
+// Funk, Hip-Hop, Pop, Trap
