7ff9d4130dd05597479c188ab52abeaa5555352f
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 11:27:00 -0300

    feat: adiciona conteúdo a arquivo metodo-sort.js

---
+++ scripts/metodo-sort.js
@@ -0,0 +1,9 @@
+/* Ordena os elementos do array */
+
+let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz'];
+
+let playlistOrdenada = playlist.sort();
+
+/* nomeVariavel.sort() */
+
+console.log("Playlist ordenada:", playlistOrdenada);
