9888d37dae9f1ca7e1cc6836bc3c375876c08d89
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 11:32:36 -0300

    feat: adiciona conteúdo a arquivo metodo-reverse.js

---
+++ scripts/metodo-reverse.js
@@ -0,0 +1,9 @@
+/* Inverte a ordem dos elementos do array */
+
+let playlist = ['Funk', 'Hip-Hop', 'Pop', 'Rock', 'Jazz'];
+
+let playlistInvertida = playlist.reverse();
+
+/* nomeVariavel.reverse() */
+
+console.log("Playlist invertida:", playlistInvertida);
