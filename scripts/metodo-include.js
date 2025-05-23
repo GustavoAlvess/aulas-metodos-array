951a9e1592a50a454989a206cc6088a4f7b65b82
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-23 10:35:50 -0300

    feat: adiciona conteúdo a arquivo metodo-includes.js

---
+++ scripts/metodo-include.js
@@ -0,0 +1,7 @@
+//Verifica se o array contém um determinado elemento
+
+let playlist = ["Funk", "HipHop", "Pop"];
+
+console.log("A playlist inclui 'Funk'?", playlist.includes('Funk')); // true
+
+console.log("A playlist inclui 'Rock'?", playlist.includes('Rock')); // false
